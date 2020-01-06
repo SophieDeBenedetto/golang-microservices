a6c246dcef89c5c82371199bbe33693a2834e7fe
sophiedebenedetto <sophie.debenedetto@gmail.com>
2020-01-06 15:20:38 -0500

    fix user_dao.go

--- mvc/domains/user_dao.go
+++ mvc/domains/user_dao.go
@@ -1,1 +1,31 @@
-mode: set
+package domains
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/SophieDeBenedetto/golang-microservices/mvc/utils"
+)
+
+var (
+	users = map[int64]*User{
+		123: {
+			ID:        123,
+			FirstName: "Sophie",
+			LastName:  "DeBenedetto",
+			Email:     "sophie@email.com",
+		},
+	}
+)
+
+// GetUser gets the user with the given ID or returns an error
+func GetUser(userID int64) (*User, *utils.ApplicationError) {
+	if user := users[userID]; user != nil {
+		return user, nil
+	}
+	return nil, &utils.ApplicationError{
+		Message: fmt.Sprintf("User %d not found", userID),
+		Status:  http.StatusNotFound,
+		Code:    "not_found",
+	}
+}
