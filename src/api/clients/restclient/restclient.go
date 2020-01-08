bb3f3f2d476b5bfca3a542fb00e24b2b21fb8173
sophiedebenedetto <sophie.debenedetto@gmail.com>
2020-01-08 17:45:15 -0500

    restclient mocks

--- src/api/clients/restclient/restclient.go
+++ src/api/clients/restclient/restclient.go
@@ -3,11 +3,48 @@ package restclient
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"net/http"
 )
 
+var (
+	enabledMocks = false
+	mocks        = make(map[string]*Mock)
+)
+
+// Mock mocks a rest client response
+type Mock struct {
+	URL        string
+	HTTPMethod string
+	Response   *http.Response
+	Error      error
+}
+
+// StartMocks sets mocking to true
+func StartMocks() {
+	enabledMocks = true
+}
+
+// StopMocks sets mocking to false
+func StopMocks() {
+	enabledMocks = false
+}
+
+// AddMock adds a mock to the mocks map
+// expects a pointer
+func AddMock(mock *Mock) {
+	mocks[mock.URL] = mock
+}
+
 // Post sends a post request to the URL with the body
 func Post(url string, body interface{}, headers http.Header) (*http.Response, error) {
+	if enabledMocks {
+		mock := mocks[url]
+		if mock == nil {
+			return nil, errors.New("No mock for URL")
+		}
+		return mock.Response, mock.Error
+	}
 	jsonBytes, err := json.Marshal(body)
 	if err != nil {
 		return nil, err
--- src/api/providers/github_provider_test.go
+++ src/api/providers/github_provider_test.go
@@ -1,7 +1,12 @@
 package githubprovider
 
 import (
+	"errors"
+	"net/http"
 	"testing"
+
+	"github.com/SophieDeBenedetto/golang-microservices/src/api/clients/restclient"
+	"github.com/SophieDeBenedetto/golang-microservices/src/api/domain/github"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -9,4 +14,17 @@ import (
 func TestGetAuthorizationHeader(t *testing.T) {
 	accessToken := "ABC123"
 	assert.EqualValues(t, "token ABC123", getAuthorizationHeader(accessToken))
+}
+
+func TestCreateRepoFailure(t *testing.T) {
+	restclient.StartMocks()
+	// & generates a pointer
+	restclient.AddMock(&restclient.Mock{
+		URL:        createRepoURL,
+		HTTPMethod: http.MethodPost,
+		Error:      errors.New("Invalid rest client response"),
+	})
+	response, err := CreateRepo("invalidToken", &github.CreateRepoRequest{})
+	assert.Nil(t, response)
+	assert.NotNil(t, err)
 }
